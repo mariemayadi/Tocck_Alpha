b22ee68fb8fdf2e2c0af758dd74875d52f0a40a9
Mariem Ayadi <mayadi@smith.edu>
2014-11-17 06:44:57 -0500

    Working FaceBook login

---
+++ Tocck/Bridge-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridge-Header.h
+//  Tocck
+//
+//  Created by Mariem Ayadi on 11/17/14.
+//  Copyright (c) 2014 Mariem Ayadi. All rights reserved.
+//
+
+#ifndef Tocck_Bridge_Header_h
+#define Tocck_Bridge_Header_h
+
+
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
---
+++ Tocck/ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  Tocck
+//
+//  Created by Mariem Ayadi on 11/17/14.
+//  Copyright (c) 2014 Mariem Ayadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, FBLoginViewDelegate {
+    
+    @IBOutlet var fbLoginView : FBLoginView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.fbLoginView.delegate = self
+        self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends"]
+    }
+    
+    //Facebook delegate methods
+    
+    func loginViewShowingLoggedInUser(loginView : FBLoginView!) {
+        println("User Logged In")
+        println("This is where you perform a segue")
+    }
+    
+    func loginViewFetchedUserInfo(loginView: FBLoginView!, user: FBGraphUser!) {
+        println("User Name: \(user.name)")
+    }
+
+    func loginViewShowingLoggedOutUser(loginView: FBLoginView!) {
+        println("User Logged Out")
+    }
+    
+    func loginView(loginView: FBLoginView!, handleError: NSError!) {
+        println("Error: \(handleError.localizedDescription)")
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
