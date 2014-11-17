a56c2798a3263c7b169143be238d9ed93bc09cc9
Mariem Ayadi <mayadi@smith.edu>
2014-11-17 07:22:18 -0500

    Add 2nd View Controller

---
+++ Tocck/myViewController2.swift
@@ -0,0 +1,23 @@
+//
+//  myViewController2.swift
+//  Tocck
+//
+//  Created by Mariem Ayadi on 11/17/14.
+//  Copyright (c) 2014 Mariem Ayadi. All rights reserved.
+//
+
+import UIKit
+
+class myViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
