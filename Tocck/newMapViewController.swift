64921dfe815ab494587d8bda440ca5283b9706b3
Mariem Ayadi <mayadi@smith.edu>
2014-12-17 00:43:14 -0500

    Add new map with pin (default Ford) - no GPS

--- Tocck/newMapViewController.swift
+++ Tocck/newMapViewController.swift
@@ -7,13 +7,39 @@
 //
 
 import UIKit
+import MapKit
 
-class newMapViewController: UIViewController {
-
+class newMapViewController: UIViewController, MKMapViewDelegate {
+    
+    
+    @IBOutlet weak var userMap: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //Have Lat and Long be global.
+        //Default is Ford Hall
+        var lat:CLLocationDegrees = 42.315904
+        var long:CLLocationDegrees = -72.637915
+        
+        //A structure that defines the area spanned by a map region.
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.075, 0.075)
+        
+        //Structure containing a geographical coordinate
+        var userLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(lat, long)
+        
+        var theRegion:MKCoordinateRegion = MKCoordinateRegionMake(userLocation, span)
+        
+        self.userMap.setRegion(theRegion, animated: true)
+        
+        var userLocationAnnotation = MKPointAnnotation()
+        
+        userLocationAnnotation.coordinate = userLocation
+        userLocationAnnotation.title = "Hi"
+        userLocationAnnotation.subtitle = "You're here!"
+        
+        self.userMap.addAnnotation(userLocationAnnotation)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,16 +47,10 @@ class newMapViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //Note: might be deleted since "back" takes care of that
+    //Button sends us back to the match page
     @IBAction func toMatchesPressed(sender: UIButton) {
-        
-        ////let myMatches = self.storyboard?.instantiateViewControllerWithIdentifier("myMatches") as myViewController4
-        
-        ////self.navigationController?.popToViewController(myMatches, animated: true)
-        
-        //self.navigationController?.popToRootViewControllerAnimated(true)
-        
-//        let myMap = self.storyboard?.instantiateViewControllerWithIdentifier("myMap") as newMapViewController
-//        self.navigationController?.pushViewController(myMap, animated: true)
+
         let myMatches = self.storyboard?.instantiateViewControllerWithIdentifier("myMatches") as myViewController4
         self.navigationController?.pushViewController(myMatches, animated: true)
     }
