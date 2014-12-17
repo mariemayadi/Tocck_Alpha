539bb04855bd505105f1c22fba4de3ef84bf0aca
Mariem Ayadi <mayadi@smith.edu>
2014-12-17 12:31:10 -0500

    Upload a distance in miles

--- Tocck/convertLatLongViewController.swift
+++ Tocck/convertLatLongViewController.swift
@@ -10,7 +10,15 @@ import UIKit
 
 class convertLatLongViewController: UIViewController {
     
+//    var matchMiles = PFObject(className:"farCloseUsers")
+    
     var queriedMatches: [String] = []
+    
+    var myLat = 42.315904
+    var myLong = -72.637915
+    
+    var latUser1 = 37.785834
+    var longUser1 = -122.406417
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +42,7 @@ class convertLatLongViewController: UIViewController {
                     NSLog("%@", lastRequest.objectId)
                     print("Last lat:")
                     //println(lastRequest["currentLat"] as String)
+                    self.myLat = lastRequest["currentLat"] as Double / 57.295779513
                 }
             } else {
                 // Log details of the failure
@@ -62,6 +71,7 @@ class convertLatLongViewController: UIViewController {
                     NSLog("%@", lastRequest.objectId)
                     print("Last long:")
                     //println(lastRequest["currentLat"] as String)
+                    self.myLong = lastRequest["currentLong"] as Double / 57.295779513
                 }
             } else {
                 // Log details of the failure
@@ -104,12 +114,72 @@ class convertLatLongViewController: UIViewController {
         
             //--------------------------------------------------
         
+        //--------------------------------------------------
+        //      USER 1 - distance update
+        //--------------------------------------------------
+        
+        var user1Query = PFQuery(className:"farClose2Users")
+        //user1Query.getObjectInBackgroundWithId(self.queriedMatches[0]) {
+        //user1Query.getObjectInBackgroundWithId("XScYmQMi5Y") {
+        user1Query.getObjectInBackgroundWithId("tLk1GcywJb") {            
+            (person: PFObject!, error: NSError!) -> Void in
+            if error == nil {
+                NSLog("%@", person)
+                self.latUser1 = person["matchLat"] as Double / 57.295779513
+                self.longUser1 = person["matchLong"] as Double / 57.295779513
+                
+                //Doing the math!
+                
+                var diffLat = self.latUser1 - self.myLat
+                var diffLong = self.longUser1 - self.myLong
+                
+                var a = pow(sin(diffLat / 2),2) + cos(self.myLat) * cos(self.latUser1) * pow(sin(diffLong / 2),2)
+                
+                var c = 2 * atan2(sqrt(a), sqrt(1-a))
+                
+                // distance in miles = R * c (where R = 3961.0 is the radius of the Earth)
+                var d = 3961.0 * c
+
+                //----------------------------------------------------
+                // Saves distance in Miles to Parse
+                //----------------------------------------------------
+                var matchMiles = PFObject(className:"farClose2Users")
+                
+                matchMiles["distFromUser"] = d as Double
+                matchMiles.saveInBackgroundWithBlock { (success, error) -> Void in
+                    if(success) {
+                        NSLog("Saved Distance to Parse")
+                    } else {
+                        // handle error
+                    }
+                }
+                
+               
+                
+            } else {
+                NSLog("%@", error)
+            }
+        }
+        
+        //-------------------------------------------------------------------------
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+//    //Saving miles from User
+//    func saveDistFromUser(){
+//        matchMiles.saveInBackgroundWithBlock { (success, error) -> Void in
+//            if(success) {
+//                NSLog("Saved Distance to Parse")
+//            } else {
+//                // handle error
+//            }
+//        }
+//    }
 
 
 }
